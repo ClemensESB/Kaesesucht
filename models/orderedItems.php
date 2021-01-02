dd2d4eff74fdea3930c2023d127e546ac08b9f26
ClemensESB <clm.st.berger@gmail.com>
2021-01-02 19:03:58 +0100

    Update orderedItems.php

--- models/orderedItems.php
+++ models/orderedItems.php
@@ -8,12 +8,12 @@ class ModelOrderedItems extends kae\core\Model
 	const TABLENAME = '`orderedItems`';
 
 	protected $schema = [
-	`id`          =>['type' => BaseModel::TYPE_INT],
-  `createdAt`   =>['type' => BaseModel::TYPE_STRING],
-  `updatetAt`   =>['type' => BaseModel::TYPE_STRING],
-  `cheese_id`   =>['type' => BaseModel::TYPE_INT],
-  `quantity`    =>['type' => BaseModel::TYPE_INT],
-  `actualPrice` =>['type' => BaseModel::TYPE_STRING],
-  `orders_id`   =>['type' => BaseModel::TYPE_INT],
+	'id'          =>['type' => BaseModel::TYPE_INT],
+  'createdAt'   =>['type' => BaseModel::TYPE_STRING],
+  'updatetAt'   =>['type' => BaseModel::TYPE_STRING],
+  'cheese_id'   =>['type' => BaseModel::TYPE_INT],
+  'quantity'    =>['type' => BaseModel::TYPE_INT],
+  'actualPrice' =>['type' => BaseModel::TYPE_STRING],
+  'orders_id'   =>['type' => BaseModel::TYPE_INT],
 	];
 }
