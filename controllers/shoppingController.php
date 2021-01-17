15c1053c81d4cdab5a33ce21541e54afad749547
ClemensESB <clm.st.berger@gmail.com>
2021-01-17 15:13:16 +0100

    Warenkorb

--- controllers/shoppingController.php
+++ controllers/shoppingController.php
@@ -21,51 +21,50 @@ class ShoppingController extends \kae\core\Controller
 
 	}
 	public function actionShoppingCart()
-	{
+	{	
+		$this->killmeTestData();
 
 		#pre_r($_POST);
-
-
 		if(isset($_SESSION['cart']))
 		{
+			if(isset($_POST['deleteProduct'])){
+				#pre_r($_POST);
+				foreach ($_SESSION['cart'] as $key => $value) 
+				{
+					if($_SESSION['cart'][$key]->__get('id') == $_POST['delID'])
+					{
+						unset($_SESSION['cart'][$key]);
+					}
+				}
+			}
 			$cart = $_SESSION['cart']; //TODO: wie werden die Items gespeichert cookie? session?
+			
 		}
 		else
 		{
 			$cart = null;
 		}
+
 		if(isset($_POST['submit']))
 		{
-			pre_r($_POST);
+			#pre_r($_POST);
+
+			foreach ($_SESSION['cart'] as $key => $value) {
+				if($_SESSION['cart'][$key]->__get('id') == $_POST['idProduct'])
+				{
+					$_SESSION['cart'][$key]->quantity = $_POST['chQuantity'];
+				}
+			}
 		}
 
 
-		$_SESSION['cart'] = array();
-		
-		$cheese1 = new Cheese(['cheeseName' => 'Gouda']);
-		$cheese2 = new Cheese(['cheeseName' => 'Butter']);
-		$cheese3 = new Cheese(['cheeseName' => 'Tilsiter']);
-		array_push($_SESSION['cart'] , $cheese1);
-		array_push($_SESSION['cart'] , $cheese2);
-		array_push($_SESSION['cart'] , $cheese3);
-		#echo('<pre>');
-		#echo($_SESSION['cart'] [0]->__get('cheeseName'));
-		#echo('</pre>');
-		$_SESSION['cart'] [0]->__set('pictureName',ASSETPATH.'pictures'.DIRECTORY_SEPARATOR.'käse.jpg');
-		$_SESSION['cart'] [0]->__set('price_id',1);
-		$_SESSION['cart'] [0]->__set('id',123);
-		$_SESSION['cart'] [0]->__set('qtyInStock',10);
-		$_SESSION['cart'] [0]->quantity = 2;
 
-		$_SESSION['cart'] [1]->quantity = 4;
-		$_SESSION['cart'] [1]->__set('id',333);
-		$_SESSION['cart'] [1]->__set('price_id',2);
-		$_SESSION['cart'] [1]->__set('qtyInStock',3);
-		$_SESSION['cart'] [2]->quantity = 7;
-		$_SESSION['cart'] [2]->__set('id',412);
-		$_SESSION['cart'] [2]->__set('price_id',3);
-		$_SESSION['cart'] [2]->__set('qtyInStock',41);
+		
 		#pre_r($_SESSION['cart']);
+
+
+
+
 	}
 	public function actionDeleteArticle(){
 
@@ -73,48 +72,42 @@ class ShoppingController extends \kae\core\Controller
 		actionShoppingCart();
 	}
 
-	public function shoppingProduct($product){
+	public function productPrice($product){
 		$price = new Price(Price::findOne('id = '.$product->__get('price_id')));
 		#var_dump($price->__get('pricePerUnit'));
-		echo($product->quantity);
+		#echo($product->quantity);
+		return $price;
+		
+	}
 
-		echo('<div class="table--object has--border panel--td">
-			<div class="column--prim panel--td has--border">
-				<div class="column--image">
-					<img src="'.$product->__get('pictureName').'" class="img--item">
-				</div>
-				<div class="table--content">
-					<p>'.$product->__get('cheeseName').'</p>
-					<p>'.$product->__get('id').'</p>
-				</div>
-				
-			</div>');
+	private function killmeTestData(){
+		$_SESSION['cart'] = array();
+		$cheese1 = new Cheese(['cheeseName' => 'Gouda','id'=>123,'price_id'=>1,'qtyInStock'=>10,]);
+		$cheese2 = new Cheese(['cheeseName' => 'Butter','id'=>333,'price_id'=>2,'qtyInStock'=>3]);
+		$cheese3 = new Cheese(['cheeseName' => 'Tilsiter','id'=>412,'price_id'=>3,'qtyInStock'=>41]);
+		array_push($_SESSION['cart'] , $cheese1);
+		array_push($_SESSION['cart'] , $cheese2);
+		array_push($_SESSION['cart'] , $cheese3);
+		#echo('<pre>');
+		#echo($_SESSION['cart'] [0]->__get('cheeseName'));
+		#echo('</pre>');
+		$_SESSION['cart'] [0]->__set('pictureName','Gouda.jpg');
 
-		echo('<div class="column--sub panel--td">');
-		echo('	<form method="POST">');
-		echo('		<select name="chQuantity" class="select-field">');
-		for ($i=1; $i <= $product->__get('qtyInStock'); $i++) {
-			echo(		'<option value="'.$i.'">'.$i.'</option>');
-		}
+		#$_SESSION['cart'] [0]->__set('id',123);
+		#$_SESSION['cart'] [0]->__set('price_id',1);	
+		#$_SESSION['cart'] [0]->__set('qtyInStock',10);
+		$_SESSION['cart'] [0]->quantity = 2;
 
-		echo('		</select>');
-		echo('	<input type="hidden" name="idProduct" value="'.$product->__get('id').'">');
-		echo('	</form>
-			</div>');
+		#$_SESSION['cart'] [1]->__set('id',333);
+		#$_SESSION['cart'] [1]->__set('price_id',2);
+		#$_SESSION['cart'] [1]->__set('qtyInStock',3);
+		$_SESSION['cart'] [1]->quantity = 4;
 
-		echo('
-			<div class="column--sub panel--td is--align-right has--border">
-			'.$price->__get('pricePerUnit').'
-			</div>
-			<div class="column--sub panel--td is--align-right has--border">
-			'.$price->__get('pricePerUnit')*$product->quantity.'
-			</div>
-			<div class="column--sub panel--td is--align-right has--border">
-			<form method="POST" action="">
-			<button type="submit" name="deleteProduct" Title="X">
-			</form>
-			</div>'
-		);
+		#$_SESSION['cart'] [2]->__set('id',412);
+		#$_SESSION['cart'] [2]->__set('price_id',3);
+		#$_SESSION['cart'] [2]->__set('qtyInStock',41);
+		$_SESSION['cart'] [2]->quantity = 7;
+
 	}
 
 }
--- views/shopping/shoppingCart.php
+++ views/shopping/shoppingCart.php
@@ -1,8 +1,8 @@
 <h1>Warenkorb</h1>
 
-<div class="panel has--border">
-	<div class="panel--body has--border">
-		<div class="table--header has--border">
+<div class="panel">
+	<div class="panel--body">
+		<div class="table--header">
 			<div class="column--prim panel--th">
 				Artikel
 			</div>
@@ -19,14 +19,69 @@
 			</div>
 		</div>
 
-		<?
-		foreach ($_SESSION['cart']  as $key => $product) {
-			$this->shoppingProduct($product);
-		}
-		#$this->shoppingProduct($_SESSION['cart'][0]);
-		?>
+
+<?
+$sum = 0.0 ;
+foreach ($_SESSION['cart']  as $key => $product) {
+	$price = $this::productPrice($product);
+	$sum += $price->__get('pricePerUnit')*$product->quantity;
+
+	echo('<div class="table--object panel--td">
+			<div class="column--prim panel--td">
+				<div class="column--image">
+					<img src="'.ASSETPATH.'images'.DIRECTORY_SEPARATOR.$product->__get('pictureName').'" class="img--item">
+				</div>
+				<div class="table--content">
+					<p>'.$product->__get('cheeseName').'</p>
+					<p>'.$product->__get('id').'</p>
+				</div>
+			</div>');
+
+	echo('	<div class="column--sub panel--td">');
+	echo('		<form method="POST" class="form" name="chQuantity">');
+	echo('			<select name="chQuantity" class="select-field">');
+					for ($i=1; $i <= $product->__get('qtyInStock'); $i++) {
+						if($product->quantity == $i)
+						{
+							echo(		'<option selected="selected" value="'.$i.'">'.$i.'</option>');
+						}
+						else
+						{
+							echo(		'<option value="'.$i.'">'.$i.'</option>');
+						}
+					}
+	echo('			</select>');
+	echo('		<button type="submit" class="btn" name="submit">-></button>
+				<input type="hidden" name="idProduct" value="'.$product->__get('id').'">');
+	echo('		</form>
+			</div>');
+
+	echo('
+			<div class="column--sub panel--td is--align-right">'.$price->__get('pricePerUnit').' €
+			</div>
+			<div class="column--sub panel--td is--align-right">'.$price->__get('pricePerUnit')*$product->quantity.' €
+			</div>
+			<div class="column--sub panel--td is--align-right">
+				<form method="POST" class="form" action="">
+				<button type="submit" class="btn" name="deleteProduct">X</button>
+				<input type="hidden" name="delID" value="'.$product->__get('id').'">
+				</form>
+			</div>
+		</div>');
+}
+
+
+?>
+		<div class="table--object panel--td">
+			
+			<?echo('Summe: '.$sum.' €');?>
+
 		</div>
+
+
+
 	</div>
 </div>
+
 
 <a href="index.php?c=shopping&a=checkout">Kasse</a>
