8a81577774b118f1768ea8fea4d796dac1388017
ClemensESB <clm.st.berger@gmail.com>
2021-02-18 16:52:16 +0100

    styles

--- views/pages/shop.php
+++ views/pages/shop.php
@@ -13,19 +13,19 @@
         <input type="hidden" name="c" value="pages">
         <input type="hidden" name="a" value="shop">
         <label for="taste">Geschmack</label>
-        <select id="taste" name="taste">
+        <select id="taste" class="select-field" name="taste">
             <option value="" <?echo(empty($_GET['taste']) ? 'selected' : '');?>>Keine Präferenz</option>
             <option value="M"<?echo(isset($_GET['taste']) && $_GET['taste'] == 'M' ? 'selected' : '');?>>Mild</option>
             <option value="W"<?echo(isset($_GET['taste']) && $_GET['taste'] == 'W' ? 'selected' : '');?>>Würzig</option>
         </select>
         <label for="lactose">Laktose</label>
-        <select id="lactose" name="lactose">
+        <select id="lactose" class="select-field" name="lactose">
             <option value="" <?echo(empty($_GET['lactose']) ? 'selected' : '');?>>Keine Präferenz</option>
             <option value="1"<?echo(isset($_GET['lactose']) && $_GET['lactose'] == 1 ? 'selected' : '');?>>Laktosefrei</option>
             <option value="2"<?echo(isset($_GET['lactose']) && $_GET['lactose'] == 2 ? 'selected' : '');?>>Nicht Laktosefrei</option>
         </select>
         <label for="milkType">Milch</label>
-        <select id="milkType" name="milkType">
+        <select id="milkType" class="select-field" name="milkType">
             <option value="" <?echo(empty($_GET['milkType']) ? 'selected' : '' );?>>Keine Präferenz</option>
             <option value="K"<?echo(isset($_GET['milkType']) && $_GET['milkType'] == 'K' ? 'selected' : '' );?>>Kuh</option>
             <option value="B"<?echo(isset($_GET['milkType']) && $_GET['milkType'] == 'B' ? 'selected' : '' );?>>Büffel</option>
@@ -33,7 +33,7 @@
             <option value="Z"<?echo(isset($_GET['milkType']) && $_GET['milkType'] == 'Z'? 'selected' : '' );?>>Ziege</option>
         </select>
         <label for="rawMilk">Rohmilch</label>
-        <select id="rawMilk" name="rawMilk">
+        <select id="rawMilk" class="select-field" name="rawMilk">
             <option value="" <?echo(empty($_GET['rawMilk']) ? 'selected' : '');?>>Keine Präferenz</option>
             <option value="1"<?echo(isset($_GET['rawMilk']) && $_GET['rawMilk'] == 1 ? 'selected' : '');?>>Ja</option>
             <option value="2"<?echo(isset($_GET['rawMilk']) && $_GET['rawMilk'] == 2 ? 'selected' : '');?>>Nein</option>
