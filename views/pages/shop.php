79d31209ba526751afa35ed66928f5dda7bd1843
Antonio_Blechschmidt <antonio.blechschmidt@web.de>
2021-02-20 18:53:21 +0100

    shop title und text entfernt

--- views/pages/shop.php
+++ views/pages/shop.php
@@ -1,14 +1,4 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-<div class ="shop_title">
-    <h1> Unsere Sorten</h1>
-    <div class = "shop_text" >
-        <p>
-            Besuchen Sie unsere/n Käseladen und erleben Sie, warum uns so viele Menschen vertrauen, wenn es um hochwertige Lebensmittel geht.<br>
-            Bei Käsesucht erhalten Sie das absolut Beste für Sie und Ihre Familie.
-        </p>
-    </div>
-</div>
 <div class = "filter_container">
     <form method="get">
         <input type="hidden" name="c" value="pages">
