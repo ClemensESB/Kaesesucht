bdcf79555437d1d237ff438e3a20e5660968e163
Antonio_Blechschmidt <antonio.blechschmidt@web.de>
2021-02-20 18:47:56 +0100

    product css 5

--- views/shopping/product.php
+++ views/shopping/product.php
@@ -1,24 +1,25 @@
-<div class="product-image-container">
-	<img class="product_image" src=<?echo(ASSETPATH.'images'.DIRECTORY_SEPARATOR.$this->fullProduct->__get('pictureName')); ?>>
-</div>
-<div class="product-info-container">
-	<div class="head-container">
-		<h1><?echo($this->fullProduct->__get('cheeseName'))?></h1>
+<div class="product">	
+	<div class="product-image-container">
+		<img class="product_image" src=<?echo(ASSETPATH.'images'.DIRECTORY_SEPARATOR.$this->fullProduct->__get('pictureName')); ?>>
 	</div>
-	<div class="text-box">
-		<p>
-			Artikelbeschreibung:<br>
-			<?echo($this->fullProduct->descrip);?>
-		</p>
-	</div>
-	<div class="text-box">
-		<p>
-			Rezept:<br>
-			<?echo($this->fullProduct->recipe);?>
-		</p>
-	</div>
-	<div class="">
-	<?$this->qtySelection($this->fullProduct,true,'in den Warenkorb legen');?>
-
+	<div class="product-info-container">
+		<div class="head-container">
+			<h1><?echo($this->fullProduct->__get('cheeseName'))?></h1>
+		</div>
+		<div class="quantity">
+			<?$this->qtySelection($this->fullProduct,true,'in den Warenkorb legen');?>
+		</div>
+		<div class="text-box">
+			<p>
+				Artikelbeschreibung:<br>
+				<?echo($this->fullProduct->descrip);?>
+			</p>
+		</div>
+		<div class="text-box">
+			<p>
+				Rezept:<br>
+				<?echo($this->fullProduct->recipe);?>
+			</p>
+		</div>
 	</div>
 </div>
