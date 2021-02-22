3ef17030ebaf95797eac3a4bcffd87f309f7f2aa
Antonio_Blechschmidt <antonio.blechschmidt@web.de>
2021-02-22 20:51:55 +0100

    product container anpassung

--- controllers/pagesController.php
+++ controllers/pagesController.php
@@ -90,8 +90,8 @@ class PagesController extends \kae\core\Controller
             echo('
 			<a href="index.php?c=shopping&a=product&id='.$product->id.'">	
 				<div class="product_container">
-					<p class="product_title">'.$product->cheeseName.'</p>
 						<img class="product_image" src="'.$path.'" alt="'.$product->cheeseName.'">
+						<p class="product_title">'.$product->cheeseName.'</p>
 						<p class ="product_descrip" >
 						Ab '.$product->pricePerUnit.' € <br>
 						<br>Verfügbarkeit : '.$product->qtyInStock.'
