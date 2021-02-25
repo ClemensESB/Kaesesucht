6f3fd622b9ced5e37f1f130d0dc14592f142e333
Antonio_Blechschmidt <antonio.blechschmidt@web.de>
2021-02-25 11:36:50 +0100

    js validierung signup

--- assets/scripts/signup.js
+++ assets/scripts/signup.js
@@ -1,5 +1,3 @@
-
-
 document.addEventListener('DOMContentLoaded', function() {
     var btnSubmit = document.getElementById('signupbutton');
     var inputEmail = document.getElementById('email');
@@ -17,7 +15,8 @@ document.addEventListener('DOMContentLoaded', function() {
         btnSubmit.addEventListener('click', function() {
             var valid = true;
             const emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-            const passRegex = /^(?=.*?[A-Z].*?)(?=.*?[a-z].*?)(?=.*[0-9].*?).{4,}$/m;
+            const passRegex = /^(?=.*?[A-Z].*?)(?=.*?[a-z].*?)(?=.*[0-9].*?).{8,}$/m;
+            const zipRegex = /^[0-9]{5}$/m;
 
             if (!inputEmail || !inputEmail.value.match(emailRegex)) {
                 valid = false;
@@ -25,8 +24,26 @@ document.addEventListener('DOMContentLoaded', function() {
             if (!inputPassword || !inputPassword1 || !(inputPassword.value.match(passRegex) && inputPassword.value == inputPassword1.value)) {
                 valid = false;
             }
-
+            if (!inputFirstName.length >= 2 && !inputFirstName.length <= 45) {
+                valid = false;
+            }
+            if (!inputLastName.length >= 2 && !inputLastName.length <= 45) {
+                valid = false;
+            }
+            if (!inputZipCode || !inputZipCode.value.match(emailRegex)) {
+                valid = false;
+            }
+            if (!inputStrNo.length >= 1 && !inputStrNo.length <= 5) {
+                valid = false;
+            }
+            if (!inputStreet.length >= 2 && !inputStreet.length <= 100) {
+                valid = false;
+            }
+            if (!inputCity.length >= 1 && !inputCity.length <= 50) {
+                valid = false;
+            }
             if (valid == false) {
+                console.log("prevented");
                 event.preventDefault();
                 event.stopPropagation();
             }
@@ -44,29 +61,37 @@ document.addEventListener('DOMContentLoaded', function() {
         inputEmail.addEventListener('keyup', function() {
             const emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
             var str = this.value;
+            var error = document.getElementById('emailError');
 
             if (str.match(emailRegex)) {
                 inputEmail.style.border = '2px solid green';
+                error.innerHTML = "";
             } else {
                 inputEmail.style.border = '2px solid red';
+                error.innerHTML = "E-mail ungültig";
             }
         });
     }
 
     if (inputPassword) {
         inputPassword.addEventListener('keyup', function() {
-            var regex1 = /^(?=.*?[A-Z].*?)(?=.*?[a-z].*?)(?=.*[0-9].*?).{4,}$/m;
-            var regex2 = /^(?=.*?[A-Z].*?)(?=.*?[a-z].*?[a-z])(?=.*[0-9].*?[0-9])(?=.*?[^\w\s].*?).{6,}$/m;
-            var regex3 = /^(?=.*?[A-Z].*?[A-Z])(?=.*?[a-z].*?[a-z])(?=.*[0-9].*?[0-9])(?=.*?[^\w\s].*?[^\w\s]).{8,}$/m;
+            const regex1 = /^(?=.*?[A-Z].*?)(?=.*?[a-z].*?)(?=.*[0-9].*?).{8,}$/m;
+            const regex2 = /^(?=.*?[A-Z].*?)(?=.*?[a-z].*?[a-z])(?=.*[0-9].*?[0-9])(?=.*?[^\w\s].*?).{8,}$/m;
+            const regex3 = /^(?=.*?[A-Z].*?[A-Z])(?=.*?[a-z].*?[a-z])(?=.*[0-9].*?[0-9])(?=.*?[^\w\s].*?[^\w\s]).{8,}$/m;
             var str = this.value;
+            var error = document.getElementById('passwordError');
             if (str.match(regex3)) {
                 inputPassword.style.border = '2px solid green';
+                error.innerHTML = "";
             } else if (str.match(regex2)) {
                 inputPassword.style.border = '2px solid blue';
+                error.innerHTML = "";
             } else if (str.match(regex1)) {
                 inputPassword.style.border = '2px solid yellow';
+                error.innerHTML = "";
             } else {
                 inputPassword.style.border = '2px solid red';
+                error.innerHTML = "mind. 1 Großbuchstaben, mind. 1 Kleinbuchstaben, mind. 1 Zahl und mind. 8 Zeichen";
             }
 
         });
@@ -77,12 +102,14 @@ document.addEventListener('DOMContentLoaded', function() {
         inputPassword1.addEventListener('keyup', function() {
             var str = this.value;
             var str2 = inputPassword.value;
+            var error = document.getElementById('password1Error');
 
             if (str == str2) {
                 inputPassword1.style.border = '2px solid green';
+                error.innerHTML = "";
             } else {
                 inputPassword1.style.border = '2px solid red';
-
+                error.innerHTML = "Passwort stimmt nicht überein";
             }
         });
     }
@@ -90,11 +117,14 @@ document.addEventListener('DOMContentLoaded', function() {
     if (inputFirstName) {
         inputFirstName.addEventListener('keyup', function() {
             var str = this.value;
+            var error = document.getElementById('firstNameError');
 
-            if (str.length >= 2) {
+            if (str.length >= 2 && str.length <= 45) {
                 inputFirstName.style.border = '2px solid green';
+                error.innerHTML = "";
             } else {
                 inputFirstName.style.border = '2px solid red';
+                error.innerHTML = "Vorname muss länger als 2 und kürzer als 46 Buchstaben sein";
 
             }
         })
@@ -103,11 +133,14 @@ document.addEventListener('DOMContentLoaded', function() {
     if (inputLastName) {
         inputLastName.addEventListener('keyup', function() {
             var str = this.value;
+            var error = document.getElementById('firstLastError');
 
-            if (str.length >= 2) {
+            if (str.length >= 2 && str.length <= 45) {
                 inputLastName.style.border = '2px solid green';
+                error.innerHTML = "";
             } else {
                 inputLastName.style.border = '2px solid red';
+                error.innerHTML = "Nachname muss länger als 2 und kürzer als 46 Buchstaben sein";
 
             }
         })
@@ -116,12 +149,14 @@ document.addEventListener('DOMContentLoaded', function() {
     if (inputStreet) {
         inputStreet.addEventListener('keyup', function() {
             var str = this.value;
+            var error = document.getElementById('streetError');
 
-            if (str.length >= 2) {
+            if (str.length >= 2 && str.length <= 100) {
                 inputLastName.style.border = '2px solid green';
+                error.innerHTML = "";
             } else {
                 inputLastName.style.border = '2px solid red';
-
+                error.innerHTML = "Straße muss länger als 2 und kürzer als 101 Buchstaben sein";
             }
         })
     }
@@ -129,12 +164,14 @@ document.addEventListener('DOMContentLoaded', function() {
     if (inputStrNo) {
         inputStrNo.addEventListener('keyup', function() {
             var str = this.value;
+            var error = document.getElementById('strNoError');
 
-            if (str.length >= 1) {
+            if (str.length >= 1 && str.length <= 5) {
                 inputLastName.style.border = '2px solid green';
+                error.innerHTML = "";
             } else {
                 inputLastName.style.border = '2px solid red';
-
+                error.innerHTML = "Straßennummer muss länger als 1 und kürzer als 6 Buchstaben sein";
             }
         })
     }
@@ -142,13 +179,15 @@ document.addEventListener('DOMContentLoaded', function() {
     if (inputZipCode) {
         inputZipCode.addEventListener('keyup', function() {
             var str = this.value;
-            var regex1 = /^[0-9]{5}$/
+            const regex1 = /^[0-9]{5}$/m;
+            var error = document.getElementById('zipCodeError');
 
             if (str.match(regex1)) {
                 inputZipCode.style.border = '2px solid green';
+                error.innerHTML = "";
             } else {
                 inputZipCode.style.border = '2px solid red';
-
+                error.innerHTML = "PLZ nicht nur aus 5 Ziffern";
             }
         })
     }
@@ -156,12 +195,14 @@ document.addEventListener('DOMContentLoaded', function() {
     if (inputCity) {
         inputCity.addEventListener('keyup', function() {
             var str = this.value;
+            var error = document.getElementById('cityError');
 
-            if (str.length >= 1) {
+            if (str.length >= 1 && str.length <= 50) {
                 inputCity.style.border = '2px solid green';
+                error.innerHTML = "";
             } else {
                 inputCity.style.border = '2px solid red';
-
+                error.innerHTML = "Ort muss länger als 1 und kürzer als 51 Buchstaben sein";
             }
         })
     }
--- views/registration/signup.php
+++ views/registration/signup.php
@@ -4,23 +4,23 @@
         <form method="POST" id="signupform" action="">
             <p class="head">Registrierung</p>
             <input type="text" value="<?echo(isset($_POST['firstName']) ? $_POST['firstName'] : '');?>" id="firstName" name="firstName" placeholder="Vorname" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Vorname'" class="inputtext">
-            <span><?echo (isset($errors['firstName']) ? $this->params['errors']['firstName'] : '');?></span>
+            <span id="firstNameError"><?echo (isset($errors['firstName']) ? $this->params['errors']['firstName'] : '');?></span>
             <input type="text" value="<?echo(isset($_POST['lastName']) ? $_POST['lastName'] : '');?>" id="lastName" name="lastName" placeholder="Nachname" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Nachname'" class="inputtext">
-            <span><?echo (isset($errors['lastName']) ? $this->params['errors']['lastName'] : '' );?></span>
+            <span id="lastNameError"><?echo (isset($errors['lastName']) ? $this->params['errors']['lastName'] : '' );?></span>
             <input type="text" value="<?echo(isset($_POST['street']) ? $_POST['street'] : '');?>" id="street" name="street" placeholder="Straße" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Straße'" class="inputtext">
-            <span><?echo (isset($errors['street']) ? $this->params['errors']['street'] : '' );?></span>
+            <span id="streetError"><?echo (isset($errors['street']) ? $this->params['errors']['street'] : '' );?></span>
             <input type="text" value="<?echo(isset($_POST['strNo']) ? $_POST['strNo'] : '');?>" id="strNo" name="strNo" placeholder="Straßennummer" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Straßennummer'" class="inputtext">
-            <span><?echo (isset($errors['strNo']) ? $this->params['errors']['strNo'] : '' );?></span>
+            <span id="strNoError"><?echo (isset($errors['strNo']) ? $this->params['errors']['strNo'] : '' );?></span>
             <input type="text" value="<?echo(isset($_POST['zipCode']) ? $_POST['zipCode'] : '');?>" id="zipCode" name="zipCode" placeholder="PLZ" onfocus="this.placeholder = ''" onblur="this.placeholder = 'PLZ'" class="inputtext">
-            <span><?echo (isset($errors['zipCode']) ? $this->params['errors']['zipCode'] : '' );?></span>
+            <span id="zipCodeError"><?echo (isset($errors['zipCode']) ? $this->params['errors']['zipCode'] : '' );?></span>
             <input type="text" value="<?echo(isset($_POST['city']) ? $_POST['city'] : '');?>" id="city" name="city" placeholder="Ort" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Ort'" class="inputtext">
-            <span><?echo(isset($errors['city']) ? $this->params['errors']['city'] : '');?></span>
+            <span id="cityError"><?echo(isset($errors['city']) ? $this->params['errors']['city'] : '');?></span>
             <input type="text" value="<?echo(isset($_POST['email']) ? $_POST['email'] : '');?>" id="email" name="email" placeholder="Email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email'" class="inputtext">
-            <span><?echo(isset($errors['email']) ? $this->params['errors']['email'] : '' );?></span>
+            <span id="emailError"><?echo(isset($errors['email']) ? $this->params['errors']['email'] : '' );?></span>
             <input type="password" id="password" name="password" placeholder="Passwort" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Passwort'" class="inputtext">
-            <span><?echo(isset($errors['password']) ? $this->params['errors']['password'] : '' );?></span>
+            <span id="passwordError"><?echo(isset($errors['password']) ? $this->params['errors']['password'] : '' );?></span>
             <input type="password" id="password1" name="password1" placeholder="Passwort wiederholen" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Passwort wiederholen'" class="inputtext">
-            
+            <span id="password1Error"><?echo(isset($errors['password1']) ? $this->params['errors']['password1'] : '' );?></span>
             <input type="submit" name="submit" id="signupbutton" value="registrieren" class="button">
         </form>
             <div class="filler"></div>
