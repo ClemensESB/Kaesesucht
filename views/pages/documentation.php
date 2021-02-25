4c3da28d04bbba498716c19d626d53d0786a6b38
riddickse <73823731+riddickse@users.noreply.github.com>
2021-02-25 01:55:13 +0100

    Doku

--- views/pages/documentation.php
+++ views/pages/documentation.php
@@ -5,6 +5,14 @@
     $path4= ASSETPATH.'images'.DIRECTORY_SEPARATOR.'Navigationstruktur.png';
     $path5= ASSETPATH.'images'.DIRECTORY_SEPARATOR.'rmDB.png';
     $path6= ASSETPATH.'images'.DIRECTORY_SEPARATOR.'Datenfluss.png';
+    $path7= ASSETPATH.'images'.DIRECTORY_SEPARATOR.'c_home.png';
+    $path8= ASSETPATH.'images'.DIRECTORY_SEPARATOR.'c_shop.png';
+    $path9= ASSETPATH.'images'.DIRECTORY_SEPARATOR.'c_product.png';
+    $path10= ASSETPATH.'images'.DIRECTORY_SEPARATOR.'c_warenkorb.png';
+    $path11= ASSETPATH.'images'.DIRECTORY_SEPARATOR.'c_kasse.png';
+    $path12= ASSETPATH.'images'.DIRECTORY_SEPARATOR.'c_profil.png';
+    $path13= ASSETPATH.'images'.DIRECTORY_SEPARATOR.'c_login.png';
+    $path14= ASSETPATH.'images'.DIRECTORY_SEPARATOR.'c_signup.png';
     ?>
 
     <h1>Beschreibung des fiktiven Kunden</h1><br><br>
@@ -18,19 +26,19 @@
     </p>
 
     <h1>Beschreibung der Websitenutzer(Zielgruppe)</h1><br><br>
-        *Demografie : <br><br>
+         <P style="text-decoration: underline;">*Demografie :</P><br><br>
         -Käseliebhaber jeden Alters<br><br>
-        *Arbeitsweise :<br><br>
+         <P style="text-decoration: underline;">*Arbeitsweise :</P><br><br>
         -nutzt Internet<br>
         -kauft lokal aber auch online<br><br>
-        *Interessen : <br><br>
+         <P style="text-decoration: underline;">*Interessen :</P><br><br>
         -isst gerne Käse, und probiert neue Rezepte mit Käse<br>
         -kocht allein, mit Freunde oder Familie<br><br>
-        *Herausforderungen : <br><br>
+        <P style="text-decoration: underline;">*Herausforderungen : </P><br><br>
         -findet nicht alle Käsesorten im lokalen Shop<br>
         -kann im lokalen Shop nicht alle Infos finden<br>
         -hat keine Zeit fürs Einkaufen<br><br>
-        *Ziele und Wünsche : <br><br>
+        <P style="text-decoration: underline;">*Ziele und Wünsche : </P><br><br>
         -möchte neue Käsesorten probieren<br>
         -möchte Käse online bestellen<br><br><br>
     </p>
@@ -44,6 +52,25 @@
     <img src="<?echo($path3);?>" alt="Seitenlayout"><br><br><br><br>
     <h1>Navigationsstruktur </h1><br><br><br><br>
     <img src="<?echo($path4);?>" alt="Navigationsstruktur"><br><br><br><br>
+    <h1>Funktionalitäten für jede Seite</h1><br><br>
+    <P style="text-decoration: underline;">Startseite</P><br><br>
+    <img src="<?echo($path7);?>" alt="home"><br><br><br><br>
+    <P style="text-decoration: underline;">Shopseite</P><br><br>
+    <img src="<?echo($path8);?>" alt="shop"><br><br><br><br>
+    <P style="text-decoration: underline;">Produktseite</P><br><br>
+    <img src="<?echo($path9);?>" alt="product"><br><br><br><br>
+    <P style="text-decoration: underline;">Warenkorb</P><br><br>
+    <img src="<?echo($path10);?>" alt="cart"><br><br><br><br>
+    <P style="text-decoration: underline;">Kasse</P><br><br>
+    <img src="<?echo($path11);?>" alt="checkout"><br><br><br><br>
+    <P style="text-decoration: underline;">Profilseite</P><br><br>
+    <img src="<?echo($path12);?>" alt="profil"><br><br><br><br>
+    <P style="text-decoration: underline;">Loginseite</P><br><br>
+    <img src="<?echo($path13);?>" alt="login"><br><br><br><br>
+    <P style="text-decoration: underline;">Signupseite</P><br><br>
+    <img src="<?echo($path14);?>" alt="signup"><br><br><br><br>
+
+
     <h1>Design </h1><br><br>
     <p>
         *Begründung zur Wahl des Layouts : Einfache Navigation, Übersichtlichkeit<br><br>
